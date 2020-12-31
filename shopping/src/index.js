f2f8557b31a88c70f90fbb804ebb65db58075b00
Remy Guts <remyguts@gmail.com>
2020-12-30 21:37:57 -0800

    switch remder app to render counter

--- shopping/src/index.js
+++ shopping/src/index.js
@@ -5,9 +5,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById("root")
 );
