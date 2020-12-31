53647ce0697b7f1d36e7b1d6f38c81993f2df1b6
Remy Guts <remyguts@gmail.com>
2020-12-30 20:30:55 -0800

    done with initial demp moving on to adding bootsrap

--- shopping/src/index.js
+++ shopping/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
 
-const element = <h1>Hello World</h1>;
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
