9b30ff9e77252097fc7f256d857d967f24f703a9
Remy Guts <remyguts@gmail.com>
2020-12-30 22:06:51 -0800

    the return of formatCount says if this is zero to print the work zero

--- shopping/src/components/counter.jsx
+++ shopping/src/components/counter.jsx
@@ -5,15 +5,15 @@ class Counter extends Component {
   };
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span>{this.state.count}</span>
         <button>Increment</button>
-      </React.Fragment>
-      );
-      
-      formatCount() {
-          const { count } = this.state;
-      }
+      </div>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
