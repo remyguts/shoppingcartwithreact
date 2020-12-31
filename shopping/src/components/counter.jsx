ca3247277a329dc45a5060ce8c18be91d5ed18f0
Remy Guts <remyguts@gmail.com>
2020-12-30 22:45:55 -0800

    added a state called styles and the properties were fontsize and font weight then put style into the span with curly braces to make it reactive and called the state using this and the properties were styles

--- shopping/src/components/counter.jsx
+++ shopping/src/components/counter.jsx
@@ -3,10 +3,17 @@ class Counter extends Component {
   state = {
     count: 0,
   };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
