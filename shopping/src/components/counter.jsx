483d9601f0e481c5c4106b16a1d204334f8157b0
Remy Guts <remyguts@gmail.com>
2020-12-30 20:36:05 -0800

    eliminate state and add h1

--- shopping/src/components/counter.jsx
+++ shopping/src/components/counter.jsx
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
- 
+
 export default Counter;
