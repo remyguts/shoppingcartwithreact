ff0d2e70a3de2428fffa9e5c2362e4c7b6cc0c66
Remy Guts <remyguts@gmail.com>
2020-12-30 22:48:30 -0800

    took out the styles attribute

--- shopping/src/components/counter.jsx
+++ shopping/src/components/counter.jsx
@@ -4,14 +4,12 @@ class Counter extends Component {
     count: 0,
   };
 
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold",
+  
   };
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span  className="badge badge-primary m-2">
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
