49d712fdb5133868f44033d6a976deaae3eba56a
Remy Guts <remyguts@gmail.com>
2021-01-02 11:58:04 -0800

    changed badge from primary to warning and added a cont classes

--- shopping/src/components/counter.jsx
+++ shopping/src/components/counter.jsx
@@ -5,9 +5,10 @@ class Counter extends Component {
   };
 
   render() {
+    const classes = "'";
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className="badge badge-warning m-2">{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
