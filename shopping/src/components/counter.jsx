8c9367dfc13a0e0a4088fc25aecdc9dcc261a25a
Remy Guts <remyguts@gmail.com>
2021-01-04 14:12:53 -0800

    bringing everything back to classes and badges for next part

--- shopping/src/components/counter.jsx
+++ shopping/src/components/counter.jsx
@@ -2,34 +2,22 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    
   };
-
+  
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          <li>
-            {this.state.tags.map((tag) => (
-              <li key={tag}>{tag}</li>
-            ))}
-          </li>
-        </ul>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
-  getBadgeClasses() {
-    let classes = "'badge m-2 badge-";
+}
+getBadgeClasses() {
+    let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
 }
 
 export default Counter;
