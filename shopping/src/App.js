64ebaa5784a9d170e4ba25fe03023042944d4f5f
Remy Guts <remyguts@gmail.com>
2021-01-04 16:05:04 -0800

    fixing increment with a constructor

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+//import counter from "./components/counter";
 function App() {
   return (
     <div className="App">
--- shopping/src/components/counter.jsx
+++ shopping/src/components/counter.jsx
@@ -1,23 +1,34 @@
 import React, { Component } from "react";
+
 class Counter extends Component {
   state = {
     count: 0,
-    
   };
-  
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  handleIncremnt() {
+    console.log("Increment Clicked", this);
+  }
+
   render() {
     return (
       <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncremnt}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
-}
-getBadgeClasses() {
+  getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
+  }
+  //export default Counter;
 }
-
-export default Counter;
